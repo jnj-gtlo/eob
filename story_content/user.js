ba5d002559d947ab10cc3c2740e7b0c086285820
wdizon <ldizon@its.jnj.com>
2020-12-03 17:01:48 +0800

    Update

--- story_content/user.js
+++ story_content/user.js
@@ -2,71 +2,47 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "6oYGWNthdwR":
+      case "5ga4BLJfUM9":
         Script1();
         break;
-      case "5m6oM6LEJhE":
+      case "5bo6zgURfoB":
         Script2();
         break;
-      case "5oEC2ZpyPAX":
+      case "5ViRGd8kJUI":
         Script3();
         break;
-      case "69uF4kaKCTi":
+      case "6iZr4pYTUN7":
         Script4();
         break;
-      case "5oxHDNdsi7S":
+      case "6OkptZmvaG6":
         Script5();
         break;
-      case "6JgS9o438ha":
+      case "5uuU6tp1Hod":
         Script6();
         break;
-      case "6PowkdvA967":
+      case "5pcmqbAdTRi":
         Script7();
         break;
-      case "5cXYMzxpNKj":
+      case "65CT2mdwSwu":
         Script8();
         break;
-      case "6lBhE6yr4K0":
+      case "6UqjxzBLs6P":
         Script9();
         break;
-      case "5ZBsghQQ8in":
+      case "6FU2xdGMHUz":
         Script10();
         break;
-      case "6WolxKorVm5":
+      case "5otrVgUOHuL":
         Script11();
         break;
-      case "5yQ6ATLVATi":
+      case "5dT303kFGJb":
         Script12();
         break;
-      case "6Gfx5JAqSqt":
+      case "6kHrYuZVE5r":
         Script13();
         break;
-      case "61wCIWBRtNH":
+      case "5itbEAdpiaP":
         Script14();
-        break;
-      case "5sihF8ibnn1":
-        Script15();
-        break;
-      case "6ppM1IfvMCE":
-        Script16();
-        break;
-      case "6YWZYmjjCON":
-        Script17();
-        break;
-      case "5dsuV5Ra4b8":
-        Script18();
-        break;
-      case "6o48Mn2Jyxg":
-        Script19();
-        break;
-      case "6YtBYEglz3E":
-        Script20();
-        break;
-      case "5WIzeU58jJ9":
-        Script21();
-        break;
-      case "6750c3fqWof":
-        Script22();
         break;
   }
 }
@@ -211,7 +187,7 @@ function add_line() {
 if(document.getElementById('bgSong')==null){
 	add_line();
 var audio = document.getElementById('bgSong');
-audio.volume = 0.2;
+audio.play();
 
 var player = GetPlayer();
 this.Location= player.GetVar("location");
@@ -243,14 +219,12 @@ function add_line() {
 if(document.getElementById('bgSong')==null){
 	add_line();
 var audio = document.getElementById('bgSong');
-audio.volume = 0.0;
 
 var player = GetPlayer();
 this.Location= player.GetVar("location");
 var audio = document.getElementById('bgSong');
 audio.src=Location+"TheEpicHero.mp3";
 audio.load();
-audio.play();
 
 }
 
@@ -297,7 +271,7 @@ function add_line() {
 if(document.getElementById('bgSong')==null){
 	add_line();
 var audio = document.getElementById('bgSong');
-audio.volume = 0.2;
+audio.play();
 
 var player = GetPlayer();
 this.Location= player.GetVar("location");
@@ -329,7 +303,6 @@ function add_line() {
 if(document.getElementById('bgSong')==null){
 	add_line();
 var audio = document.getElementById('bgSong');
-audio.volume = 0.0;
 
 var player = GetPlayer();
 this.Location= player.GetVar("location");
@@ -354,162 +327,6 @@ function Script14()
 {
   var audio = document.getElementById('bgSong');
 audio.pause(); 
-
-}
-
-function Script15()
-{
-  //load the scripts dynamically into the head of the document
-function add_line() {
-    var line = document.createElement("audio");
-    var head=document.getElementsByTagName('body')[0];
-    line.type = "audio/mp3";
-    line.src="";
-    line.id="bgSong" ;
-        line.autoplay = true;
-        line.loop = true;
-    head.appendChild(line);
-}
-
-//but we only want to add these once!
-if(document.getElementById('bgSong')==null){
-	add_line();
-var audio = document.getElementById('bgSong');
-audio.volume = 0.2;
-
-var player = GetPlayer();
-this.Location= player.GetVar("location");
-var audio = document.getElementById('bgSong');
-audio.src=Location+"TheEpicHero.mp3";
-audio.load();
-audio.play();
-
-}
-
-
-}
-
-function Script16()
-{
-  //load the scripts dynamically into the head of the document
-function add_line() {
-    var line = document.createElement("audio");
-    var head=document.getElementsByTagName('body')[0];
-    line.type = "audio/mp3";
-    line.src="";
-    line.id="bgSong" ;
-        line.autoplay = true;
-        line.loop = true;
-    head.appendChild(line);
-}
-
-//but we only want to add these once!
-if(document.getElementById('bgSong')==null){
-	add_line();
-var audio = document.getElementById('bgSong');
-audio.volume = 0.0;
-
-var player = GetPlayer();
-this.Location= player.GetVar("location");
-var audio = document.getElementById('bgSong');
-audio.src=Location+"TheEpicHero.mp3";
-audio.load();
-audio.play();
-
-}
-
-
-}
-
-function Script17()
-{
-  var audio = document.getElementById('bgSong');
-audio.volume = 0.2;
-
-}
-
-function Script18()
-{
-  var audio = document.getElementById('bgSong');
-audio.volume = 0.0;
-
-}
-
-function Script19()
-{
-  //load the scripts dynamically into the head of the document
-function add_line() {
-    var line = document.createElement("audio");
-    var head=document.getElementsByTagName('body')[0];
-    line.type = "audio/mp3";
-    line.src="";
-    line.id="bgSong" ;
-        line.autoplay = true;
-        line.loop = true;
-    head.appendChild(line);
-}
-
-//but we only want to add these once!
-if(document.getElementById('bgSong')==null){
-	add_line();
-var audio = document.getElementById('bgSong');
-audio.volume = 0.2;
-
-var player = GetPlayer();
-this.Location= player.GetVar("location");
-var audio = document.getElementById('bgSong');
-audio.src=Location+"TheEpicHero.mp3";
-audio.load();
-audio.play();
-
-}
-
-
-}
-
-function Script20()
-{
-  //load the scripts dynamically into the head of the document
-function add_line() {
-    var line = document.createElement("audio");
-    var head=document.getElementsByTagName('body')[0];
-    line.type = "audio/mp3";
-    line.src="";
-    line.id="bgSong" ;
-        line.autoplay = true;
-        line.loop = true;
-    head.appendChild(line);
-}
-
-//but we only want to add these once!
-if(document.getElementById('bgSong')==null){
-	add_line();
-var audio = document.getElementById('bgSong');
-audio.volume = 0.0;
-
-var player = GetPlayer();
-this.Location= player.GetVar("location");
-var audio = document.getElementById('bgSong');
-audio.src=Location+"TheEpicHero.mp3";
-audio.load();
-audio.play();
-
-}
-
-
-}
-
-function Script21()
-{
-  var audio = document.getElementById('bgSong');
-audio.volume = 0.2;
-
-}
-
-function Script22()
-{
-  var audio = document.getElementById('bgSong');
-audio.volume = 0.0;
 
 }
 
